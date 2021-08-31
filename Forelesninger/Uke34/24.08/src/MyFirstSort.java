dfc204b8f4d4fedf331e5f9ac8539ce8e9c931f3
MichalPajestka <s354558@oslomet.no>
2021-08-31 11:13:52 +0200

    Lagrer testen

--- Forelesninger/Uke34/24.08/src/MyFirstSort.java
+++ Forelesninger/Uke34/24.08/src/MyFirstSort.java
@@ -1,25 +1,30 @@
 public class MyFirstSort {
 
     public static void main(String[] args) {
-
         int[] values = {1, 7, 2, 4, 6, 9};
+
+        myFirstSort(values);
+
+    }
+
+    public static void myFirstSort(int[] values){
 
         for (int k=0; k<values.length-1; ++k) {
 
 
-        int max_index = findMax(values, k, values.length);
-        System.out.println("Største verdi ligger på plass "
-                + max_index + " og har verdi " + values[max_index]);
+            int max_index = findMax(values, k, values.length);
+           // System.out.println("Største verdi ligger på plass "
+             //       + max_index + " og har verdi " + values[max_index]);
 
-        //bytte plass på tall på plass 0 og max_index
-        int temp = values[k];
-        values[k] = values[max_index];
-        values[max_index] = temp;
+            //bytte plass på tall på plass 0 og max_index
+            int temp = values[k];
+            values[k] = values[max_index];
+            values[max_index] = temp;
 
-        System.out.println("Arrayet etter ombytting");
-        for (int i=0; i<values.length; ++i){
-            System.out.print(values[i] + ", ");
-        }
+           // System.out.println("Arrayet etter ombytting");
+            for (int i=0; i<values.length; ++i){
+           //     System.out.print(values[i] + ", ");
+            }
         }
     }
 
@@ -31,7 +36,7 @@ public class MyFirstSort {
 
         //Sjekk at grensene fo løkken er riktig
         // [1, values.lengt)
-        for (int i=0; i<values.length; ++i) {
+        for (int i=fra+1; i<til; ++i) {
             //sjekk om vi har funnet nytt største tall
             if (values[i] > max_value) {
                 max_value = values[i];
---
+++ Forelesninger/Uke34/24.08/src/MyFirstSortTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyFirstSortTest {
+
+    @org.junit.jupiter.api.Test
+    void myFirstSort() {
+        int[] values = {1, 7, 2, 4, 6, 9};
+
+        MyFirstSort.myFirstSort(values);
+
+        int truth[] = {9, 7, 6, 5, 2, 1};
+        assertArrayEquals(truth, values);
+    }
+
+    @org.junit.jupiter.api.Test
+    void findMax() {
+        int[] values = {1, 7, 2, 4, 6, 9};
+        int index = MyFirstSort.findMax(values, 0, values.length);
+
+        assertEquals(5, index);
+        assertEquals(9, values[index]);
+
+
+    }
+}
